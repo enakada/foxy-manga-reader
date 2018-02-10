98db0e6f1fa8370bbd56e35f4538178bf2928af4
Edson Nakada <6558490+enakada@users.noreply.github.com>
2018-02-10 14:40:48 -0200

    Add kissmanga.com content scripts

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ const scriptList = [
   './src/content_scripts/mangafox.js',
   './src/content_scripts/mangaeden.js',
   './src/content_scripts/mangahere.js',
+  './src/content_scripts/kissmanga.js',
 ];
 
 
